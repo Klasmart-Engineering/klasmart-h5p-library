58b17a531fc15e0a0e19235a767384c8a483d033
Shawn Lee <shawn.lee@calmid.com>
2020-05-11 12:33:53 +0900

    fix(security): introduce React to fix editor XSS

--- src/renderers/player.tsx
+++ src/renderers/player.tsx
@@ -1,22 +1,38 @@
-export default (model) => `<!doctype html>
-<html class="h5p-iframe">
-<head>
-    <meta charset="utf-8">
-    
-    ${model.styles
-        .map((style) => `<link rel="stylesheet" href="${style}"/>`)
-        .join('\n    ')}
-    ${model.scripts
-        .map((script) => `<script src="${script}"></script>`)
-        .join('\n    ')}
+// tslint:disable-next-line: no-submodule-imports
+import ReactDOMServer from 'react-dom/server';
+import React, { useState, useEffect } from "react";
+import { IPlayerModel } from "../types"
 
-    <script>
-        H5PIntegration = ${JSON.stringify(model.integration, null, 2)};
-    </script>
-    ${model.customScripts}
-</head>
-<body>
-    <div class="h5p-content" data-content-id="${model.contentId}"></div>
-    <a href="${model.downloadPath}">Download</button>
-</body>
-</html>`;
+// tslint:disable-next-line: function-name
+function Player(props: any):any {
+    // console.log("render")
+    // console.log(props.model.integration)
+
+    return (
+        <html className="h5p-iframe">
+            <head>
+                <meta charSet="utf-8" />
+                {/* <script src="http://localhost:8000/record.js" /> */}
+                { props.model.styles.map((style) => (
+                    <link rel="stylesheet" href={style} />
+                ))}
+
+                { props.model.scripts.map((script) => (
+                    <script src={script} />
+                ))}
+
+                <script dangerouslySetInnerHTML={{__html: `H5PIntegration = ${JSON.stringify(props.model.integration, null, 2)}`}} />
+                
+                { props.model.customScripts.map((script) => (
+                    <script src={script} />
+                ))}
+            </head>
+            <body>
+                <div className="h5p-content" data-content-id={props.model.contentId}></div>
+                <a href={props.model.downloadPath}>Download</a>
+            </body>
+        </html>
+    );
+}
+
+export default (model: IPlayerModel) => `${ReactDOMServer.renderToString(<Player model={model} />)}`

a2d7012753c3404bf4beaf0365cfc7858c7687de
cabauman <cab452005@gmail.com>
2022-02-10 13:48:29 +0900

    style: npm run format

--- tests/unit/packageLockVersion.test.ts
+++ tests/unit/packageLockVersion.test.ts
@@ -1,8 +1,8 @@
-import { expect } from 'chai'
-import * as pack from '../../package-lock.json'
+import { expect } from 'chai';
+import * as pack from '../../package-lock.json';
 
 describe('packageLockVersion', () => {
-  it('package-lock.json lockfileVersion equals 2', () => {
-    expect(pack.lockfileVersion).to.equal(2, 'Please use node 16.')
-  })
-})
+    it('package-lock.json lockfileVersion equals 2', () => {
+        expect(pack.lockfileVersion).to.equal(2, 'Please use node 16.');
+    });
+});

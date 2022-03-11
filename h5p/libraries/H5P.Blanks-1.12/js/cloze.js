edb66efe93954d65637490cecd8406777511cd9d
Oliver Tacke <o.tacke@posteo.de>
2022-03-11 11:05:04 +0100

    Revert "feat(H5P-516): make cloze width reflect solution width"
    This reverts commit 6a24dcd85c6f2747d6170a171b8271711181277d.

--- h5p/libraries/H5P.Blanks-1.12/js/cloze.js
+++ h5p/libraries/H5P.Blanks-1.12/js/cloze.js
@@ -64,14 +64,6 @@
     };
 
     /**
-     * Get solutions.
-     * @return {string[]} Solutions.
-     */
-    this.getSolutions = function () {
-      return answers;
-    };
-
-    /**
      * Check if filled out.
      *
      * @param {boolean}
@@ -164,7 +156,6 @@
      */
     this.setInput = function ($element, afterCheck, afterFocus, clozeIndex, totalCloze) {
       $input = $element;
-      $input.data('clozeIndex', clozeIndex);
       $wrapper = $element.parent();
       inputLabel = inputLabel.replace('@num', (clozeIndex + 1))
         .replace('@total', totalCloze);

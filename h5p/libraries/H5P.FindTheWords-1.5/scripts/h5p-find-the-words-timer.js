79406d8bb383f838f040fab2f8440f8415c5cb93
Oliver Tacke <o.tacke@posteo.de>
2022-05-07 17:16:54 +0200

    feat(H5P-608): add restoring state

--- h5p/libraries/H5P.FindTheWords-1.5/scripts/h5p-find-the-words-timer.js
+++ h5p/libraries/H5P.FindTheWords-1.5/scripts/h5p-find-the-words-timer.js
@@ -5,15 +5,19 @@
    * @class H5P.FindTheWords.Timer
    * @extends H5P.Timer
    * @param {H5P.jQuery} $element
+   * @param {number} previousTimeMs Previous time in ms.
    */
-  FindTheWords.Timer = function ($element) {
+  FindTheWords.Timer = function ($element, previousTimeMs, callbacks) {
     /** @alias H5P.FindTheWords.Timer# */
     const that = this;
+
     // Initialize event inheritance
     Timer.call(that, 1000);
 
     /** @private {string} */
     const naturalState = '0:00';
+
+    callbacks = callbacks || {};
 
     /**
      * update - Set up callback for time updates.
@@ -30,14 +34,25 @@
         seconds = '0' + seconds;
       }
       $element.text(minutes + ':' + seconds);
+
+      if (callbacks.store) {
+        callbacks.store();
+      }
     };
 
     // Setup default behavior
     that.notify('every_tenth_second', update);
+
     that.on('reset', function () {
       $element.text(naturalState);
       that.notify('every_tenth_second', update);
     });
+
+    // Restore previous time
+    if (previousTimeMs) {
+      this.setClockTime(previousTimeMs);
+      update();
+    }
   };
 
   // Inheritance

de501d712b75ee1cd4da22f4480f1a9849bea4b3
Oliver Tacke <o.tacke@posteo.de>
2022-07-08 14:37:49 +0200

    feat(H5P-719): add height limiter for KLL

--- h5p/libraries/H5P.GuessTheAnswer-1.5/guess-the-answer.js
+++ h5p/libraries/H5P.GuessTheAnswer-1.5/guess-the-answer.js
@@ -31,13 +31,93 @@ H5P.GuessTheAnswer = (function () {
    */
   function createMediaElement(params, contentId, instance) {
     var element = document.createElement('div');
+
+    let displayLimits = (
+      instance.isRoot() &&
+      H5P.KLDisplay && H5P.KLDisplay.computeDisplayLimitsKLL
+    ) ?
+      H5P.KLDisplay.computeDisplayLimitsKLL(instance.rootElement.parentNode) :
+      null;
+
+    if (
+      displayLimits &&
+      params.library && params.library.split(' ')[0] === 'H5P.Video' &&
+      params.params && params.params.sources && params.params.sources.length &&
+      params.params.sources[0].path && params.params.sources[0].path.indexOf('youtube.com') !== -1
+    ) {
+      params.visuals = params.visuals || {};
+      params.visuals.fit = false
+    }
+
     var mediaInstance = H5P.newRunnable(params, contentId, H5P.jQuery(element), true);
+
+    if (displayLimits) {
+      instance.on('resize', function () {
+        limitMediaHeight(instance, mediaInstance, displayLimits);
+      });
+    }
 
     // Resize this instance, on video resize, and vise versa
     instance.on('resize', triggerResize(mediaInstance));
     mediaInstance.on('resize', triggerResize(instance));
 
     return element;
+  }
+
+  /**
+   * Limit media height.
+   * @param {H5P.ContentType} instance This.
+   * @param {H5P.ContentType} mediaInstance Instance of medium.
+   * @param {object} displayLimits Display limits.
+   */
+  function limitMediaHeight(instance, mediaInstance, displayLimits) {
+    // Determine height of answer title to be deduced from available height
+    var answerTitle = instance.rootElement.querySelector('.h5p-guess-answer-title');
+    var answerTitleHeight = 0;
+    if (answerTitle) {
+      var answerTitleStyle = window.getComputedStyle(answerTitle);
+      answerTitleHeight = answerTitle.offsetHeight +
+        parseFloat(answerTitleStyle.getPropertyValue('margin-top')) +
+        parseFloat(answerTitleStyle.getPropertyValue('margin-bottom'));
+    }
+
+    // Determine height of show solution button to be deduced from available height
+    var showSolutionButton = instance.rootElement.querySelector('.show-solution-button');
+    var showSolutionButtonHeight = 0;
+    if (showSolutionButton) {
+      var showSolutionButtonStyle = window.getComputedStyle(showSolutionButton);
+      showSolutionButtonHeight = showSolutionButton.offsetHeight +
+        parseFloat(showSolutionButtonStyle.getPropertyValue('margin-top')) +
+        parseFloat(showSolutionButtonStyle.getPropertyValue('margin-bottom'));
+    }
+
+    // Determine available height
+    var availableHeight = Math.max(0, displayLimits.height - answerTitleHeight - showSolutionButtonHeight);
+
+    // Set max width that would allow available height
+    var maxWidth = '';
+    if (mediaInstance.libraryInfo && mediaInstance.libraryInfo.machineName === 'H5P.Image') {
+      maxWidth = (mediaInstance.height && mediaInstance.height > 0) ?
+        availableHeight * mediaInstance.width / mediaInstance.height :
+        '100%';
+    }
+    else if (mediaInstance.libraryInfo && mediaInstance.libraryInfo.machineName === 'H5P.Video') {
+      var video = instance.rootElement.querySelector('.media .h5p-video');
+      if (video) {
+        maxWidth = video.offsetHeight > 0 ?
+          availableHeight * video.offsetWidth / video.offsetHeight :
+          '100%';
+
+        // YouTube needs one extra resize when ready
+        if (video.classList.value.indexOf('h5p-youtube') !== -1) {
+          mediaInstance.once('ready', () => {
+            instance.trigger('resize');
+          });
+        }
+      }
+    }
+
+    instance.rootElement.querySelector('.media').style.maxWidth = (maxWidth === '') ? maxWidth : maxWidth + 'px';
   }
 
   /**
